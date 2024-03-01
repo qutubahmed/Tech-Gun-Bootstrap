2c73af8231ba4e8492a12f0610e7debd7f88f6ca
qutub ahmed <qutubahmed104@gmail.com>
2024-03-01 23:41:39 +0500

    add javascript file

---
+++ app.js
@@ -0,0 +1,14 @@
+var age = +prompt("what is your age?" , "my age is:");
+if (age === 23) {
+    alert ("23 saal ke logon ko \n khush amdeed");
+}
+
+else if (age < 23) {
+    alert ("bachhe mana hai");
+}
+
+else {
+    alert ("aap ki padaish pe lanat hai");
+}
+
+
